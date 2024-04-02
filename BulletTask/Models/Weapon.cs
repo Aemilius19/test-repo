5ba3939938b938d5392d517353f98a232edfc5ef
ElnurBX <tu8dhdtwy@code.edu.az>
2024-04-02 09:17:34 +0400

    Initial commit

---
+++ BulletTask/Models/Weapon.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulletTask.Models
+{
+    internal class Weapon
+    {
+        byte _capacitybullet;
+        public byte Capacitybullet
+        {
+            get { return _capacitybullet; }
+            set
+            {
+                if (value > 0)
+                {
+                    _capacitybullet = value;
+                }
+            }
+        }
+        public byte CurrentBullet;
+        bool _firemod;
+        public bool Firemod
+        {
+            get { return _firemod; }
+            set
+            {
+                if (value == false)
+                {
+                    _firemod = value;
+                }
+                else if (value == true)
+                {
+                    _firemod = value;
+                }
+            }
+
+
+        }
+        public void Shoot ()
+        {
+            if (Firemod==false)
+            {
+                CurrentBullet -= 1;
+                Console.WriteLine("tekli atiw rejimi");
+                Console.WriteLine("<---------->");
+                Console.WriteLine("Gulle ugurla atildi");
+                Console.WriteLine($"{CurrentBullet} dene  gulle qaldi");
+            }
+            else if(Firemod==true)
+            {
+                Console.WriteLine("avtomatik atiw rejimi");
+                Console.WriteLine("<---------->");
+                for (int i = CurrentBullet; i !=0; i--)
+                {
+                    CurrentBullet-= 1;
+                    Console.WriteLine("Gulle ugurla atildi");
+                    Console.WriteLine($"{CurrentBullet} dene  gulle qaldi");
+                }
+            }
+        }
+        public byte Reload()
+        {
+            CurrentBullet = Capacitybullet;
+            Console.WriteLine($"Silah reload olundu indi gulle sayi {CurrentBullet}");
+            return CurrentBullet;
+        }
+
+        public int GetRemainBullet() 
+        {
+            int leftcapacity = Capacitybullet - CurrentBullet;
+            Console.WriteLine($"daragin dolmasi uchun {leftcapacity} geder gulle lazimdi");
+            return leftcapacity;
+            
+        }
+
+        public void ChangeFireMod(bool value)
+        {
+            if (Firemod==false&&value==true) { Firemod=value; Console.WriteLine("silah avtomatik rejime kecdi"); }
+            else if (Firemod==true&&value==false) { Firemod = value; Console.WriteLine("silah tekli rejime kecdi"); }
+            else
+            {
+                Console.WriteLine("Firemod deyiwilmedi deyerler sehf verilib ve ya rejim duzgun secilmiyib");
+            }
+
+        }
+        public Weapon(byte capacitybullet,  byte currentBullet, bool firemod)
+        {  
+            Capacitybullet = capacitybullet;
+            CurrentBullet = currentBullet;
+            Firemod = firemod;
+        }
+        public void Showallinfo()
+        {
+            Console.WriteLine($"weapon capacity - {Capacitybullet},Weapom current bullet - {CurrentBullet}");
+        }
+    }
+}
---
+++ BulletTask/Program.cs
@@ -0,0 +1,77 @@
+﻿using BulletTask.Models;
+
+namespace BulletTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon newweapon = new Weapon(7,7,false);
+            Console.WriteLine("|=========Welcome=========|");
+            bool exit=false;
+            do
+            {
+                Console.WriteLine("|====Choose operation====|");
+                Console.WriteLine("1-Show weapon info");
+                Console.WriteLine("2-To shoot");
+                Console.WriteLine("3-Get Remain Bullet");
+                Console.WriteLine("4-Reload");
+                Console.WriteLine("5-Change weapon firemod");
+                Console.WriteLine("0-Exit");
+                Console.WriteLine("|=======|");
+                Console.WriteLine("choose option");
+                string input=Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        newweapon.Showallinfo();
+                        if (newweapon.Firemod==false)
+                        {
+                            Console.WriteLine("Tekli atiw rejimi");
+                        }
+                        else if(newweapon.Firemod==true)
+                        {
+                            Console.WriteLine("Avtomatik atiw rejimi");
+                        }
+                        Console.WriteLine("|============|");
+                        break;
+                    case "2":
+                        newweapon.Shoot();
+                        break;
+                    case "3":
+                        newweapon.GetRemainBullet();
+                        break;
+                    case "4":
+                        newweapon.Reload();
+                        break;
+                    case "5":
+                        Console.WriteLine("Choose firemod");
+                        Console.WriteLine("1-Tekli");
+                        Console.WriteLine("2-Avtomatik");
+                        string changechoice= Console.ReadLine();
+                        
+                        switch (changechoice)
+                        {
+                            case "1":
+                                bool choicebool1 = true;
+                                newweapon.ChangeFireMod(choicebool1);
+                                break;
+                            case "2":
+                                bool choicebool2 = false;
+                                newweapon.ChangeFireMod(choicebool2);
+                                break;
+                            default:Console.WriteLine("duzgen secin");
+                                break;
+                        }
+                        break;
+                    case "0":
+                        exit = true;
+                        break;
+                    default: Console.WriteLine("choose the right option");
+                        break;
+                }
+
+            } while (exit!=true);
+        }
+    }
+}
